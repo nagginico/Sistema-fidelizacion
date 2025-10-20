ef391cc5a3be82661d6e59b3b7dfce4db852db14
nagginico <nagginico@gmail.com>
2025-10-19 23:28:12 -0300

    Cambios deploy

--- fid/fid/settings.py
+++ fid/fid/settings.py
@@ -86,21 +86,13 @@ WSGI_APPLICATION = 'fid.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 DATABASE_URL = os.environ.get("DATABASE_URL")
-if DATABASE_URL:
-    DATABASES = {
-        "default": dj_database_url.config(
-            default=DATABASE_URL,
-            conn_max_age=600,  # mantiene la conexión viva
-            ssl_require=True,  # Render exige SSL
-        )
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
