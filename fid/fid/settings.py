c9bc89d1bf1584d6c83b00d813b1c55c2fb1c340
nagginico <nagginico@gmail.com>
2025-10-19 22:02:29 -0300

    Problemas al cerrar sesion, con login_url se deberia solucionar

--- fid/fid/settings.py
+++ fid/fid/settings.py
@@ -34,6 +34,7 @@ DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 ALLOWED_HOSTS = ['*']
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+LOGIN_URL = '/accounts/login/'
 
 
 # Application definition
