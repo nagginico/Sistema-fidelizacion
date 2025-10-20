a2d7505a899810ad8959dc8f41408555f546f2a8
nagginico <nagginico@gmail.com>
2025-10-19 23:50:10 -0300

    admin error 3

--- fid/create_superuser.py
+++ fid/create_superuser.py
@@ -1,17 +1,16 @@
 from django.contrib.auth import get_user_model
-from django.db.utils import IntegrityError
 
 User = get_user_model()
 
-username = "naggi"
-email = "nagginico@gmail.com"
-password = 44033699
+def ensure_superuser():
+    username = "admin"
+    email = "admin@example.com"
+    password = "admin1234"
 
-try:
     if not User.objects.filter(username=username).exists():
         User.objects.create_superuser(username=username, email=email, password=password)
-        print("Superusuario creado con éxito.")
+        print("Superusuario creado correctamente.")
     else:
         print("ℹEl superusuario ya existe.")
-except IntegrityError:
-    print("No se pudo crear el superusuario (posible duplicado).")
+
+ensure_superuser()
--- fid/fid/settings.py
+++ fid/fid/settings.py
@@ -86,13 +86,23 @@ WSGI_APPLICATION = 'fid.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 DATABASE_URL = os.environ.get("DATABASE_URL")
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+if os.environ.get("RENDER"):
+    # Render usa DATABASE_URL (PostgreSQL)
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=os.environ.get("DATABASE_URL"),
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    # Local (SQLite)
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
